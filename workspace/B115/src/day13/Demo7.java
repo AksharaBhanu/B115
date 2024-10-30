18e2f63e25d9b3e812f9ce9e5cd14701e23562c2
AksharaBhanu <javagalbhanu@gmail.com>
2024-10-30 20:34:16 +0530

    Day13

---
+++ workspace/B115/src/day13/Demo7.java
@@ -0,0 +1,14 @@
+package day13;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Demo7 {
+
+	public static void main(String[] args)  {
+		SimpleDateFormat dateFormat=new SimpleDateFormat("dd_MMMMM_yyyy_hh_mm_ss");
+		String timestamp = dateFormat.format(new Date());
+		System.out.println(timestamp);
+	}
+
+}
