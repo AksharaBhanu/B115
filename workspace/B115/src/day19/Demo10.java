acc4595551090fa0a03634c711988f446105d55a
AksharaBhanu <javagalbhanu@gmail.com>
2024-11-11 20:26:19 +0530

    Day19

---
+++ workspace/B115/src/day19/Demo10.java
@@ -0,0 +1,27 @@
+package day19;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Demo10 {
+
+	public static void main(String[] args) {
+		WebDriver driver=new ChromeDriver();
+		driver.get("file:///E:/B115%207PM%20Selenium/share/pages/Sample4.html");
+		WebElement link1 = driver.findElement(By.tagName("a"));
+		System.out.println(link1.getAttribute("href"));
+		
+		List<WebElement> allLinks = driver.findElements(By.tagName("a"));
+		for(WebElement link:allLinks)
+		{
+			System.out.println(link.getAttribute("href"));
+		}
+		
+		driver.quit();
+	}
+
+}
