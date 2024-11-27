3c3887c295acf81c071e8ba844bdef062b27c1b0
AksharaBhanu <javagalbhanu@gmail.com>
2024-11-27 20:29:40 +0530

    Day29

---
+++ workspace/B115/src/day29/Demo6.java
@@ -0,0 +1,25 @@
+package day29;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class Demo6 {
+
+	public static void main(String[] args) throws MalformedURLException {
+		String gridURL="https://oauth-crakhi.c2021-c306a:af9500f0-dafd-4e54-a787-49e332972909@ondemand.eu-central-1.saucelabs.com:443/wd/hub";
+		URL url=new URL(gridURL);
+		ChromeOptions options=new ChromeOptions();
+		
+		WebDriver driver=new RemoteWebDriver(url,options);
+		driver.get("https://aksharatraining.com/");
+		System.out.println(driver.getTitle());
+		driver.quit();
+
+	}
+
+}
