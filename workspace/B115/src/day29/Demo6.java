dbcdeddf9e05da6efe7e68e7a4b4cd7d942b64e2
AksharaBhanu <javagalbhanu@gmail.com>
2024-11-28 20:33:20 +0530

    Day30

--- workspace/B115/src/day29/Demo6.java
+++ workspace/B115/src/day29/Demo6.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class Demo6 {
 
-	public static void main(String[] args) throws MalformedURLException {
+	public static void main(String[] args) throws Exception {
 		String gridURL="https://oauth-crakhi.c2021-c306a:af9500f0-dafd-4e54-a787-49e332972909@ondemand.eu-central-1.saucelabs.com:443/wd/hub";
 		URL url=new URL(gridURL);
 		ChromeOptions options=new ChromeOptions();
@@ -19,7 +19,7 @@ public class Demo6 {
 		driver.get("https://aksharatraining.com/");
 		System.out.println(driver.getTitle());
 		driver.quit();
-
+		
 	}
 
 }
